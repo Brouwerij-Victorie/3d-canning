8ef31e454d1aeb1e75b0c8b0dfad7764a0545876
Maurits Meester <maurits@e-mmer.nl>
2022-03-10 17:44:33 +0100

    :tada: (demo) Basic canning demo

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <model-viewer
+    src="VIC_Croptop_WebCanTest-2.gltf"
+    camera-controls
+    ar
+    ar-modes="scene-viewer webxr"
+    class="can__viewer"
+    skybox-image="aircraft_workshop_01_1k.hdr"
+  ></model-viewer>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+.can__viewer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
